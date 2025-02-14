6628b3ce0e96a78649a1bf4ea2e2425e92d6acf9
xiaolufacai <xiaolufacai3@outlook.com>
2025-02-14 14:55:01 +0800

    index

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -8,7 +8,8 @@ class Index extends BaseController
 {
     public function index()
     {
-        return '<style>*{ padding: 0; margin: 0; }</style><iframe src="https://www.thinkphp.cn/welcome?version=' . \think\facade\App::version() . '" width="100%" height="100%" frameborder="0" scrolling="auto"></iframe>';
+        return redirect('/admin/login/index')->send();
+//        return '<style>*{ padding: 0; margin: 0; }</style><iframe src="https://www.thinkphp.cn/welcome?version=' . \think\facade\App::version() . '" width="100%" height="100%" frameborder="0" scrolling="auto"></iframe>';
     }
 
     public function hello($name = 'ThinkPHP8')

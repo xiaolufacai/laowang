4e830e1378788347b7435759d6156f1d4a8d60f9
zclee <3883688@gmail.com>
2025-11-28 14:45:10 +0800

    APPS

--- app/middleware/CorsMiddleware.php
+++ app/middleware/CorsMiddleware.php
@@ -1,20 +1,33 @@
 <?php
+declare (strict_types=1);
 
 namespace app\middleware;
 
+use think\Request;
+use think\Response;
+
 class CorsMiddleware {
+    /**
+     * 处理请求
+     *
+     * @param Request $request
+     * @param \Closure $next
+     * @return Response
+     */
     public function handle($request, \Closure $next) {
         $response = $next($request);
         $response->header([
-            'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With',
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'GET,POST,PUT,DELETE,OPTIONS',
+            'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
             'Access-Control-Allow-Credentials' => 'true',
         ]);
 
-        // 处理 OPTIONS 预检请求
-        if ($request->method(true) === 'OPTIONS') {
-            return response('', 204);
+        // 处理 OPTIONS 预检请求直接返回 204
+        if ($request->isOptions()) {
+            $response->code(204);
+            $response->send();
+            exit;
         }
 
         return $response;

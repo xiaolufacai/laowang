5c424e78988ceafd9cbb25ee448e33ad2dfb1fca
zclee <3883688@gmail.com>
2025-11-28 15:04:28 +0800

    APPS

--- app/index/route/app.php
+++ app/index/route/app.php
@@ -10,4 +10,4 @@
 // +----------------------------------------------------------------------
 use think\facade\Route;
 
-Route::get('/apps', 'index/AppController/apps')->middleware(\app\middleware\CorsMiddleware::class);
+Route::get('/apps', 'AppController/apps')->middleware(\app\middleware\CorsMiddleware::class);

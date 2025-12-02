fe49c70f0cd2142470246799a6686f7ac0917e13
zclee <3883688@gmail.com>
2025-12-02 09:39:29 +0800

    execute

---
+++ app/command/Pk.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace app\command;
+
+use think\console\Command;
+use think\console\Input;
+use think\console\Output;
+use think\console\input\Argument;
+
+class Pk extends Command {
+    protected function configure() {
+        $this->setName('test')
+            ->addArgument('action', Argument::OPTIONAL, 'action name')
+            ->addArgument('params', Argument::IS_ARRAY, 'params');
+    }
+
+    protected function execute(Input $input, Output $output) {
+        // 获取子命令
+        $action = $input->getArgument('action');
+        $params = $input->getArgument('params');
+
+        if (!$action) {
+            return $output->writeln("Missing action");
+        }
+
+        // 兼容 test:detail 写法
+        if (strpos($action, ':') !== false) {
+            [, $action] = explode(':', $action);
+        }
+
+        if (!method_exists($this, $action)) {
+            return $output->writeln("Action [$action] not found");
+        }
+
+        try {
+            $result = call_user_func_array([$this, $action], $params);
+
+            if ($result !== null) {
+                $output->writeln($result);
+            }
+
+        } catch (\Throwable $e) {
+            $output->writeln("Error: " . $e->getMessage());
+        }
+    }
+
+    // ====================
+    // 子命令方法放这
+    // ====================
+    protected function detail($id, $type = 'default') {
+        return "detail => id={$id}, type={$type}";
+    }
+
+
+    protected function clear() {
+        return "clear executed";
+    }
+}
--- app/controller/Index.php
+++ app/controller/Index.php
@@ -4,16 +4,9 @@ namespace app\controller;
 
 use app\BaseController;
 
-class Index extends BaseController
-{
-    public function index()
-    {
+class Index extends BaseController {
+    public function index() {
         return redirect('/admin/login/index')->send();
 //        return '<style>*{ padding: 0; margin: 0; }</style><iframe src="https://www.thinkphp.cn/welcome?version=' . \think\facade\App::version() . '" width="100%" height="100%" frameborder="0" scrolling="auto"></iframe>';
-    }
-
-    public function hello($name = 'ThinkPHP8')
-    {
-        return 'hello,' . $name;
     }
 }

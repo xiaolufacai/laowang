db7520010224a811b3b496ac4823d2e08ae05ebc
zclee <3883688@gmail.com>
2026-01-24 09:21:04 +0800

    开启任务

--- config/const.php
+++ config/const.php
@@ -6,6 +6,9 @@ $channels = [
     'huawei'  => '华为',
     'xiaomi'  => '小米',
     'yyb'     => '应用宝',
+    'cool'    => '酷安',
+    'oneplus' => '一加',
+    'other'   => '其他',
 ];
 return [
     'channels' => $channels,
